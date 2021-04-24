e73cc22d8f72cae80eb01cfa9b4bc5a0c256dbdf
Diego Rivera <diego@cuatro.studio>
2021-04-24 23:36:33 +0200

    Add main note window

--- Tildone/Source/AppDelegate.swift
+++ Tildone/Source/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  tildone
+//  Tildone
 //
 //  Created by Diego on 24/4/21.
 //
@@ -13,27 +13,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = Note()
 
         // Create the window and set the content view.
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .borderless],
             backing: .buffered, defer: false)
+        window.level = .floating
+        window.titlebarAppearsTransparent = true
         window.isReleasedWhenClosed = false
         window.center()
-        window.setFrameAutosaveName("Main Window")
+        window.setFrameAutosaveName("Main note")
         window.contentView = NSHostingView(rootView: contentView)
         window.makeKeyAndOrderFront(nil)
+        window.backgroundColor = #colorLiteral(red: 1, green: 0.9834647775, blue: 0.7855550647, alpha: 1)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
+/// Avoid focus ring on every text field all over the app
+extension NSTextField {
+    open override var focusRingType: NSFocusRingType {
+        get { .none }
+        set { }
+    }
+}
