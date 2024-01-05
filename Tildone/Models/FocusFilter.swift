f8281a37a85460ae5a6528efec196dcb1c73041f
Diego Rivera <diego@cuatro.studio>
2024-01-05 02:50:19 +0100

    Focus filter for notes being able to stay in the background

---
+++ Tildone/Models/FocusFilter.swift
@@ -0,0 +1,50 @@
+//
+//  FocusFilter.swift
+//  Tildone
+//
+//  Created by Diego Rivera on 5/1/24.
+//
+
+import AppIntents
+
+struct FocusFilter: SetFocusFilterIntent {
+
+    static var title: LocalizedStringResource = "Set contents visibility"
+    static var description: IntentDescription = """
+    Choose how to limit your notes' content visibility based on this Focus
+    """
+    
+    var displayRepresentation: DisplayRepresentation {
+        var title: String?
+        switch (tastTextBlurred, noteMayStayBackground) {
+        case (false, false):
+            title = "Task text visible on a note that stays in the foreground"
+        case (true, true):
+            title = "Task text blurred on a note that may stay in the background"
+        case (true, _):
+            title = "Task text blurred"
+        case (_, true):
+            title = "Note may stay in the background"
+        }
+        return DisplayRepresentation(stringLiteral: title!)
+    }
+    
+    @Parameter(title: "Task text blurred", default: false)
+    var tastTextBlurred: Bool
+    
+    @Parameter(title: "Note may stay in the background", default: false)
+    var noteMayStayBackground: Bool
+    
+    static var openAppWhenRun: Bool = true
+    
+    var appContext: FocusFilterAppContext {
+        return FocusFilterAppContext()
+    }
+    
+    func perform() async throws -> some IntentResult {
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: .visibility, object: (tastTextBlurred, noteMayStayBackground))
+        }
+        return .result()
+    }
+}
