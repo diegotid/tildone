4e849e85591b0ff15a7d7faec135b95a51989bc7
Diego Rivera <diego@cuatro.studio>
2024-03-19 00:00:43 +0100

    Ensure retrocompatibility for first app versions which had unindexed todo lists

--- Tildone/Models/Todo.swift
+++ Tildone/Models/Todo.swift
@@ -39,13 +39,33 @@ final class Todo {
 
 extension Array where Element == Todo {
     func sorted() -> [Todo] {
-        self.sorted(by: { $0.created < $1.created })
-            .sorted(by: { $0.index ?? Int.max < $1.index ?? Int.max })
+        self.secondaryOrderSorted()
+            .mainOrderSorted()
+            .indexIfUnindexed()
     }
     
     func maxIndex() -> Int? {
         return self.reduce(0) { max, task in
             (task.index ?? 0) > max ?? 0 ? task.index : max
         }
+    }
+    
+    private func mainOrderSorted() -> [Todo] {
+        self.sorted(by: { $0.index ?? Int.max < $1.index ?? Int.max })
+    }
+    
+    private func secondaryOrderSorted() -> [Todo] {
+        self.sorted(by: { $0.created < $1.created })
+    }
+    
+    /// Function intended for retrocompatibility as first app versions had unindexed todo lists
+    private func indexIfUnindexed() -> [Todo] {
+        self.enumerated()
+            .map { index, todo in
+                if todo.index == nil {
+                    todo.index = index
+                }
+                return todo
+            }
     }
 }
