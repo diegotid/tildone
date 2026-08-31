5614aced4a4ab47511fc2382461ebd8e4c1c8765
Diego Rivera <diego@cuatro.studio>
2026-08-31 15:58:24 +0200

    Add new note menu shortcut back

--- Tildone/App/Lifecycle/TildoneApp.swift
+++ Tildone/App/Lifecycle/TildoneApp.swift
@@ -122,6 +122,7 @@ struct TildoneApp: App {
                 Button("New Note") {
                     NotificationCenter.default.post(name: .new, object: nil)
                 }
+                .keyboardShortcut("n")
                 Button(foregroundNoteID != nil ? "Discard Empty Note" : "Close window") {
                     NotificationCenter.default.post(name: .close, object: nil)
                 }
